d0bccf8cf07446b3ac3e45337e71da9de9a61478
Daniel Dahme <dahme@qualitus.de>
2026-06-02 22:12:42 +0200

    ilias 12 ready; readme added

--- plugin.php
+++ plugin.php
@@ -3,6 +3,6 @@
 $id = 'Base3MermaidPC';
 $version = '4.0.0';
 $ilias_min_version = '10.0';
-$ilias_max_version = '11.999';
+$ilias_max_version = '12.999';
 $responsible = 'Daniel Dahme';
 $responsible_mail = 'info@base3.de';
